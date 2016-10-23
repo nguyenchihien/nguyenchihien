fdd9e14d9934a54a293bd4f01bbc4ff4c1b34f07
nguyenchihien <hiennc@onlikeservices.com>
2016-10-23 10:51:34 +0700

    1051

---
+++ assets/setting.js
@@ -0,0 +1,72 @@
+// ============= HOME ==============
+var Page = {
+    //-- de trong "" de giu nguyen default text
+    logoText: "Lý Hồng Lang ",
+    subHeading: "Website để thể hiện. <strong>Landing page</strong> để thu hút khách hàng",
+    mainHeading: "Tôi muốn đem <b>BHNT</b> đến với <b>mọi người</b>",
+    CTA: "Tôi muốn có ngay Landing Page",
+    brief: "Cơ hội nhận discount or Free Landing Page. Chỉ cần theo dõi các chương trình cộng đồng của chúng tôi",
+
+    // ============= VARS ==============
+    url : "http://nguyenchihien.chinhlatoi.top/", //-- co slash cuoi /
+    mainkeyWord : "nguyen chi hien",
+    longkeyWords : ["Email Marketing", "Landing Page", "IT services"],
+    facebookAppId : "303370230021409", //-- your appID
+    googleAnalyticId : "UA-82197343-2", //-- your id
+    formId: "https://docs.google.com/forms/d/e/1FAIpQLScjBXL-M0eRB0qhX1xSV5sWWAwsXbNqtjAGm3aq0-l5EZAisg/viewform?embedded=true",
+}
+
+var Social = {
+    //-- de trong "" de giu nguyen default text
+    email: "maito:hiennc@onlikeservices.com",
+    facebook: "facebook.com/kevin.nguyen.927980",
+    twitter: "twitter.com",
+    google: "google.com",
+    instagram: "instagram.com",
+    pinterest: "pinterest.com",
+    linkedin: "linkedin.com/hien-nguyen-681639117"
+}
+
+var colors = [
+    "red","blue","green","orange",  // 0 1 2 3  
+    "pink", "purple", "light-blue", "cyan", // 4 5 6 7  
+    "deep-purple", "indigo", "teal", "lime", // 8 9 10 11
+    "light-green", "yellow", "amber", "deep-orange", // 12 13 14 15
+    "brown", "grey", "blue-grey"      // 16 17 18 19
+];
+
+colors = ['light-blue']
+var i = Math.floor( Math.random() * (colors.length - 0) + 0 );
+var chooseColor = colors[i].toString();
+
+
+// ============= PERSONAL ==============
+var Person = {
+    //-- de trong "" de giu nguyen default text
+    logoText: "Nguyễn Chí Hiển",
+    fullname: "<strong>Nguyễn Chí Hiển</strong>.",
+    slogan: "Website, IT services",
+    sex: '<i class="fa male"></i>', //-- male, female
+    phone: 'tel:0906838741',
+    // message: "Tôi đã sẵn sàng, còn bạn thì sao?", //-- thong diep chinh hien nay!
+    message: "Luôn sẵn sàng làm hài lòng các khách hàng",
+    
+    email: "mailto:hiennc@onlikeservices.com",
+    facebook: 'https://facebook.com/kevin.nguyen.927980',
+    facebook: "https://facebook.com/kevin.nguyen.927980",
+    twitter: "twitter.com",
+    google: "google.com",
+    instagram: "instagram.com",
+    pinterest: "pinterest.com",
+    linkedin: "https://www.linkedin.com/in/hien-nguyen-681639117",
+    CTA: "Tôi muốn có ngay Landing Page",
+}
+
+
+var Social = {
+
+}
+
+
+
+    
